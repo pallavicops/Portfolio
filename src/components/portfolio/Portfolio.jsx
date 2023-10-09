cfbafcd5792d25ce0c32cbd42c6202daf7f3ec27
Pallavi Kumari <75251279+pallavicops@users.noreply.github.com>
2023-10-09 22:52:38 +0530

    initial update

--- src/components/portfolio/Portfolio.jsx
+++ src/components/portfolio/Portfolio.jsx
@@ -4,6 +4,7 @@ import IM2 from '../../assets/p2.png'
 import IM3 from '../../assets/p3.png'
 import IM4 from '../../assets/p4.png'
 import IM5 from '../../assets/p5.png'
+
 import './portfolio.css'
 
 function Portfolio() {
@@ -54,6 +55,18 @@ function Portfolio() {
          <img src={IM4} alt="" />
         </div>
         <h3>ClipBoard Activity</h3>
+        <div className="portfolio_item-cta">
+           
+           <a href="https://github.com/pallavicops/ClipBoardActivity" className='btn'>Github</a>
+           <a href="https://github.com/pallavicops/ClipBoardActivity" className='btn btn-primary' target='blank'>Live Demo</a>
+  
+          </div>
+      </article>
+      <article className='portfolio_item'>
+        <div className="porifolio_item-image">
+         <img src={IM5} alt="" />
+        </div>
+        <h3>Currency Converter</h3>
         <div className="portfolio_item-cta">
            
            <a href="https://github.com/pallavicops/ClipBoardActivity" className='btn'>Github</a>
